204c4bca27ea4f7088f4a9e690fc65b58bcb9295
Thomas Fouquert <tfouquert@gmail.com>
2023-01-06 19:25:31 +0100

    update

---
+++ src/data/dataMeeting.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    id: 1,
+    day: "Mon",
+    date: 3,
+    hours: ["9:00 AM", "2:00 PM", "4:00 PM"],
+  },
+  {
+    id: 2,
+    day: "Tue",
+    date: 4,
+    hours: ["10:30 AM", "1:00 PM", null],
+  },
+  {
+    id: 3,
+    day: "Wed",
+    date: 5,
+    hours: ["9:00 AM", "3:00 PM", "6:00 PM"],
+  },
+  {
+    id: 4,
+    day: "Thu",
+    date: 6,
+    hours: ["11:00 AM", null, "3:50 PM"],
+  },
+  {
+    id: 5,
+    day: "Fri",
+    date: 7,
+    hours: ["9:20 AM", "1:15 PM", "5:45 PM"],
+  },
+];
