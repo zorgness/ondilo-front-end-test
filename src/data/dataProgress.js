cbf540361d7434c10e6f956790f4badade0e6d5a
Thomas Fouquert <tfouquert@gmail.com>
2023-01-09 23:26:10 +0100

    update

--- src/data/dataProgress.js
+++ src/data/dataProgress.js
@@ -2,6 +2,7 @@ export const data = {
   labels: [],
   datasets: [
     {
+      cutout: "60%",
       label: ["illustration"],
       data: [35, 65],
       backgroundColor: ["rgba(255,85,41,255)", "rgba(59,33,50,255)"],
