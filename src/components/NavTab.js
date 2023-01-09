ac2fd9499099d9e014386db10f915e616a197e4f
Thomas Fouquert <tfouquert@gmail.com>
2023-01-10 00:04:45 +0100

    update

--- src/components/NavTab.js
+++ src/components/NavTab.js
@@ -1,28 +1,39 @@
 import React from "react";
 
+const Tab = ({ label, onClick, isActive }) => {
+  return (
+    <li className="">
+      <a
+        href="/"
+        className={`tab-underlined ${isActive ? "active" : ""}`}
+        onClick={(event) => onClick(event, label)}
+      >
+        {label}
+      </a>
+    </li>
+  );
+};
+
 const NavTab = () => {
+  const labels = ["Recent Added", "In Progress", "In Review ", " Completed"];
+  const [currentLabel, setCurrentLabel] = React.useState(labels[0]);
+  const handleClick = (event, label) => {
+    event.preventDefault();
+    setCurrentLabel(label);
+  };
+
   return (
     <ul className="list-inline tabs-underlined m-4 justify-content-between">
-      <li>
-        <a href="/" className="tab-underlined active">
-          Recent Added
-        </a>
-      </li>
-      <li>
-        <a href="/" className="tab-underlined">
-          In Progress
-        </a>
-      </li>
-      <li>
-        <a href="/" className="tab-underlined">
-          In Review
-        </a>
-      </li>
-      <li>
-        <a href="/" className="tab-underlined">
-          Completed
-        </a>
-      </li>
+      {labels.map((label, index) => {
+        return (
+          <Tab
+            key={index}
+            label={label}
+            isActive={label === currentLabel}
+            onClick={handleClick}
+          />
+        );
+      })}
     </ul>
   );
 };
