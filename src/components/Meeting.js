0840b98b82732ff22ddbe2cca068ca8b29773237
Thomas Fouquert <tfouquert@gmail.com>
2023-01-09 13:39:59 +0100

    update

--- src/components/Meeting.js
+++ src/components/Meeting.js
@@ -4,9 +4,9 @@ import { data } from "../data/dataMeeting";
 
 const thStyle = {
   background: "#fee5df",
+
   borderRadius: "10px",
-  height: "72px",
-  width: "72px",
+  padding: "10px 0",
 };
 
 const Meeting = () => {
@@ -50,79 +50,93 @@ const Meeting = () => {
         </div>
       </div>
 
-      <div
-        style={{ background: "#fef9f5", padding: "16px", borderRadius: "10px" }}
+      <Table
+        borderless={true}
+        style={{
+          background: "#fef9f5",
+          borderRadius: "10px",
+          padding: "16px",
+          // borderSpacing: "24px 8px",
+          // borderCollapse: "collapse",
+          // overflow: "hidden",
+          // display: "table",
+        }}
+        className="text-center"
       >
-        <Table
-          borderless={true}
-          style={{ background: "#fef9f5" }}
-          className="text-center"
-        >
-          <thead>
-            <tr>
-              {data.map(({ id, day, date }) => {
-                return (
-                  <th
-                    style={thStyle}
-                    key={id}
-                    className={`col-table${id}`}
-                    onMouseEnter={() => handleMouseEnter(id)}
-                    onMouseLeave={() => handleMouseLeave(id)}
-                  >
-                    {day}
-                    <br />
-                    <span>{date}</span>
-                  </th>
-                );
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              {data.map(({ id, hours }) => {
-                return (
-                  <td
-                    key={id}
-                    className={`col-table${id}`}
-                    onMouseEnter={() => handleMouseEnter(id)}
-                    onMouseLeave={() => handleMouseLeave(id)}
-                  >
-                    {hours[0] ?? "_"}
-                  </td>
-                );
-              })}
-            </tr>
-            <tr>
-              {data.map(({ id, hours }) => {
-                return (
-                  <td
-                    key={id}
-                    className={`col-table${id}`}
-                    onMouseEnter={() => handleMouseEnter(id)}
-                    onMouseLeave={(e) => handleMouseLeave(id, e)}
-                  >
-                    {hours[1] ?? "_"}
-                  </td>
-                );
-              })}
-            </tr>
-            <tr>
-              {data.map(({ id, hours }) => {
-                return (
-                  <td
-                    key={id}
-                    className={`col-table${id}`}
-                    onMouseEnter={() => handleMouseEnter(id)}
-                    onMouseLeave={() => handleMouseLeave(id)}
-                  >
-                    {hours[2] ?? "_"}
-                  </td>
-                );
-              })}
-            </tr>
-          </tbody>
-        </Table>
-      </div>
+        {/* <colgroup>
+          <col class="bg-success"></col>
+          <col span="2" class="bg-info"></col>
+          <col span="2" class="bg-danger"></col>
+        </colgroup> */}
+
+        <thead>
+          <tr>
+            {data.map(({ id, day, date }) => {
+              return (
+                <th
+                  style={thStyle}
+                  className={`col-table${id}`}
+                  key={id}
+                  onMouseEnter={() => handleMouseEnter(id)}
+                  onMouseLeave={() => handleMouseLeave(id)}
+                >
+                  {day}
+                  <br />
+                  <span>{date}</span>
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {data.map(({ id, hours }) => {
+              return (
+                <td
+                  key={id}
+                  className={`col-table${id}`}
+                  onMouseEnter={() => handleMouseEnter(id)}
+                  onMouseLeave={() => handleMouseLeave(id)}
+                >
+                  {hours[0] ?? "_"}
+                </td>
+              );
+            })}
+          </tr>
+          <tr>
+            {data.map(({ id, hours }) => {
+              return (
+                <td
+                  key={id}
+                  className={`col-table${id}`}
+                  onMouseEnter={() => handleMouseEnter(id)}
+                  onMouseLeave={(e) => handleMouseLeave(id, e)}
+                >
+                  {hours[1] ?? "_"}
+                </td>
+              );
+            })}
+          </tr>
+          <tr>
+            {data.map(({ id, hours }) => {
+              return (
+                <td
+                  key={id}
+                  className={`col-table${id}`}
+                  style={{
+                    borderBottomRightRadius: "10px",
+                    borderBottomLeftRadius: "10px",
+                  }}
+                  onMouseEnter={() => handleMouseEnter(id)}
+                  onMouseLeave={() => handleMouseLeave(id)}
+                >
+                  {hours[2] ?? "_"}
+                </td>
+              );
+            })}
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 };
